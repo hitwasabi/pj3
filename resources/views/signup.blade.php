e5bb3e03cbd206ff780aa178efd5376bccac6f75
PhucVipPr <vuquangphuc004@gmail.com>
2023-06-20 16:03:15 +0700

    update login

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -44,37 +44,38 @@
                                 <div class="tab active-tab" id="tab-1">
                                     <div class="inner-box">
                                         <h4>Đăng ký</h4>
-                                        <form action="{{url('/saveAccount')}}" method="post" class="default-form">
+                                        <form action="{{url('/register')}}" method="post" class="default-form">
                                             @csrf
                                             <div class="form-group">
                                                 <label>Tên đăng nhập</label>
-                                                <input type="text" name="name" required="Nhập tên đăng nhập của bạn">
+                                                <input type="text" name="name" required>
                                             </div>
                                             <div class="form-group">
                                                 <label>Email</label>
-                                                <input type="email" name="email" required="Nhập email của bạn">
+                                                <input type="email" name="email" required>
                                             </div>
                                             <div class="form-group">
                                                 <label>Phone</label>
-                                                <input type="number" name="phone" required="Nhập số điện thoại của bạn">
+                                                <input type="number" name="phone" required>
                                             </div>
                                             <div class="form-group">
                                                 <label>Mật khẩu</label>
-                                                <input type="password" name="password" required="Nhập mật khẩu của bạn">
+                                                <input type="password" name="password" required>
                                             </div>
                                             <div class="form-group">
                                                 <label>Xác nhận mật khẩu</label>
-                                                <input type="password" name="password" required="Nhập lại mật khẩu của bạn">
+                                                <input type="password" name="password_confirmation" required>
                                             </div>
                                             <div class="form-group message-btn">
                                                 <button type="submit" class="theme-btn btn-one">Đăng ký</button>
                                             </div>
                                         </form>
                                         <div class="othre-text">
-                                            <p>Đã có sẵn tài khoản? <a href="signin.blade.php">Đăng nhập</a></p>
+                                            <p>Đã có sẵn tài khoản? <a href="{{url('/login')}}">Đăng nhập</a></p>
                                         </div>
                                     </div>
                                 </div>
+
                                 <div class="tab" id="tab-2">
                                     <div class="inner-box">
                                         <h4>Sign up</h4>
