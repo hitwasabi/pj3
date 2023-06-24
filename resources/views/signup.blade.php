808927340f4d6b5c30595a3d82b25975919cb151
hitwasabi <anhnamco9@gmail.com>
2023-06-24 10:22:14 +0700

    update linh tinh

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -34,12 +34,6 @@
                             <h2>Đăng ký với ChipHome</h2>
                         </div>
                         <div class="tabs-box">
-                            <div class="tab-btn-box">
-                                <ul class="tab-btns tab-buttons centred clearfix">
-                                    <li class="tab-btn active-btn" data-tab="#tab-1">Chủ trọ</li>
-                                    <li class="tab-btn" data-tab="#tab-2">Admin</li>
-                                </ul>
-                            </div>
                             <div class="tabs-content">
                                 <div class="tab active-tab" id="tab-1">
                                     <div class="inner-box">
@@ -76,35 +70,6 @@
                                     </div>
                                 </div>
 
-                                <div class="tab" id="tab-2">
-                                    <div class="inner-box">
-                                        <h4>Sign up</h4>
-                                        <form action="http://azim.commonsupport.com/Realshed/signin.html" method="post" class="default-form">
-                                            <div class="form-group">
-                                                <label>User name</label>
-                                                <input type="text" name="name" required="">
-                                            </div>
-                                            <div class="form-group">
-                                                <label>Email address</label>
-                                                <input type="email" name="email" required="">
-                                            </div>
-                                            <div class="form-group">
-                                                <label>New Password</label>
-                                                <input type="password" name="name" required="">
-                                            </div>
-                                            <div class="form-group">
-                                                <label>Confirm Password</label>
-                                                <input type="password" name="name" required="">
-                                            </div>
-                                            <div class="form-group message-btn">
-                                                <button type="submit" class="theme-btn btn-one">Sign up</button>
-                                            </div>
-                                        </form>
-                                        <div class="othre-text">
-                                            <p>Already have an account? <a href="signin.blade.php">Sign in</a></p>
-                                        </div>
-                                    </div>
-                                </div>
                             </div>
                         </div>
                     </div>
