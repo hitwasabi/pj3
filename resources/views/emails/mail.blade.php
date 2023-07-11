ddd3a5a1108682f8fd72d2710d32aebf31fea902
PhucVipPr <vuquangphuc004@gmail.com>
2023-07-11 16:52:33 +0700

    update

--- resources/views/emails/mail.blade.php
+++ resources/views/emails/mail.blade.php
@@ -8,7 +8,7 @@
 <p>{{ $mailData['body'] }}</p>
 
 <p>Cảm ơn vì đã tin tưởng và mua hàng trên trang web của chúng tôi</p>
-   Trong tương lai đội ngũ phát triển CodeToànBug sẽ cố gắng hơn nữa!
+   Trong tương lai đội ngũ phát triển trang web CodeToànBug sẽ cố gắng hơn nữa!
 
 <p>Xin chân thành cảm ơn!</p>
 </body>
