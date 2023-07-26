9e687a2fd9cdcaf260c3176a3a61c97eb139dee2
hitwasabi <anhnamco9@gmail.com>
2023-07-27 01:35:58 +0700

    update

--- resources/views/agents/wrong-level.blade.php
+++ resources/views/agents/wrong-level.blade.php
@@ -6,16 +6,21 @@
     <div id="main-wrapper">
     @include('admin.preloader')
     @include('agents.header')
-
-        <div style="align-content: center" class="content-body">
-            <form action="{{url('client/home/pricing')}}" method="GET">
-                @csrf
-                <h1>Xin lỗi!</h1>
-                <h3><p>Hiện tại bạn chưa thể đăng bài do chưa thanh toán gói.</p></h3>
-                <h3><p>Để sử dụng được chức năng này, hãy đến thanh toán gói!</p></h3>
-                <button type="submit" class="btn btn-xs btn-outline-secondary">Tới mua gói</button>
-            </form>
+        <div class="content-body">
+            <!-- row -->
+            <div class="container-fluid">
+                <div class="">
+                        <form action="{{url('client/home/pricing')}}" method="GET">
+                            @csrf
+                            <h1>Xin lỗi!</h1>
+                            <a><p>Hiện tại bạn chưa thể đăng bài do chưa thanh toán gói.</p></a>
+                            <a><p>Để sử dụng được chức năng này, hãy đến thanh toán gói!</p></a>
+                            <button type="submit" class="btn btn-xs btn-outline-secondary">Tới mua gói</button>
+                        </form>
+                </div>
+            </div>
         </div>
+
 
 
 
